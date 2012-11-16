f03aaffb86415ce2b344d6eb976b7f1445c4efd5
Samet Alparslan <cellf1sh@Samet-Alparslans-MacBook-Pro.local>
2012-11-16 00:31:52 +0000

    swipepinch.html
    <!--
    Touch-based browser technique to swipe and pinch using JS
    
    By Samet Kohen
    -->
    
    <!DOCTYPE htm>
    <head>
    <meta charset=UTF-8" />
    <title>Swipe Gesture Detection</title>
    <meta name="viewport" content="width=device-width; initial-scale=1.0;
    maximum-scale=1.0; user-scalable=0;">
    <script type="text/javascript" src="swipe.js"></script> <script
    type="text/javascript">
    window.onload = function() {
    var swipev = new MobiSwipe("vertical"); swipev.direction =
    swipev.VERTICAL; swipev.onswipedown = function() { alert('down'); };
    swipev.onswipeup = function() { alert('up'); };
    var swipeh = new MobiSwipe("horizontal"); swipeh.direction =
    swipeh.HORIZONTAL; swipeh.onswiperight = function() { alert('right');
    }; swipeh.onswipeleft = function() { alert('left'); };
    } </script> </head> <body>
    <div style="width: 100%; height: 150px; background-color: blue"
    id="vertical"> Vertical Swipe
    </div>
    <div style="width: 100%; height: 150px; background-color: red"
    id="horizontal"> Horizontal Swipe
    </div>
    </body> </html>

---
+++ swipepinch.js
@@ -0,0 +1,74 @@
+/**
+Creates a swipe gesture event handler
+
+By Samet Kohen
+*/
+
+function MobiSwipe(id) {
+    //Constants
+    this.HORIZONTAL = 1;
+    this.VERTICLE = 2;
+    this.AXIS_THRESHOLD = 30;
+    this.GESTURE_DELTA = 60; //The min delta in the axis to fire the gesture
+
+this.direction = this.HORIZONTAL; 
+this.element = document.getElementById(id); 
+this.onswiperight = null;
+this.onswiperleft = null;
+this.onswipeup = null;
+this.onswipedown = null;
+this.inGesture = false;
+
+// Private members
+this._originalX = 0
+this._originalY = 0
+var _this = this;
+// Makes the element clickable on iPhone 
+this.element.onclick = function() {void(0)};
+
+var mousedown = function(event) { 
+// Finger press
+event.preventDefault();
+_this.inGesture = true;
+_this._originalX = (event.touches) ? event.touches[0].pageX : event.pageX; _this._originalY = (event.touches) ? event.touches[0].pageY : event.pageY; 
+// Only for iPhone
+if (event.touches && event.touches.length!=1) {
+_this.inGesture = false; // Cancel gesture on multiple touch }
+};
+
+var mousemove = function(event) { 
+// Finger moving
+event.preventDefault(); var delta = 0;
+
+// Get coordinates using iPhone or standard technique
+var currentX = (event.touches) ? event.touches[0].pageX : event.pageX; var currentY = (event.touches) ? event.touches[0].pageY : event.pageY;
+
+// Checking if the user is still in line with the axis if (_this.inGesture) {
+if ((_this.direction==_this.HORIZONTAL)) {
+delta = Math.abs(currentY-_this._originalY);
+} else {
+delta = Math.abs(currentX-_this._originalX); }
+if (delta >_this.AXIS_THRESHOLD) {
+// Cancel gesture, the user is moving in the other axis
+_this.inGesture = false; }
+}
+// Check if we can consider it a swipe if (_this.inGesture) {
+if (_this.direction==_this.HORIZONTAL) {
+delta = Math.abs(currentX-_this._originalX); if (currentX>_this._originalX) {
+direction = 0; } else {
+direction = 1; }
+} else {
+delta = Math.abs(currentY-_this._originalY); if (currentY>_this._originalY) {
+direction = 2; } else {
+direction = 3; }
+}
+if (delta >= _this.GESTURE_DELTA) { 
+// Gesture detected!
+var handler = null; switch(direction) {
+case 0: handler = _this.onswiperight; break; case 1: handler = _this.onswipeleft; break; case 2: handler = _this.onswipedown; break; case 3: handler = _this.onswipeup; break;
+}
+if (handler!=null) {
+// Call to the callback with the optional delta
+handler(delta); }
+_this.inGesture = false; }
+} };
